99a4fca83ba01bfe42c41a235127a5a67f6be4dd
Codemonk-adi <adityachhitarka2001@gmail.com>
2022-01-30 00:01:04 +0530

    added consolelog

--- controllers/admin.js
+++ controllers/admin.js
@@ -32,6 +32,7 @@ exports.secureparser = async(req, res) => {
 
     function create_process(file) {
         return new bbPromise((resolve, reject) => {
+            console.dir(file)
             var c_process = spawn('python', ["./pythonCode/main.py",
                 file.url,
                 file.filetype,
